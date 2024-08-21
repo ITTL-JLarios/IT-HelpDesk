6415e3354aecd2cf76454b4c8e8612c8cd1df5df
ittl-jlarios <jared@totaltransportlogistics.us>
2024-08-21 11:12:39 -0600

    chore: users route without db

--- backend/api/routes/users.py
+++ backend/api/routes/users.py
@@ -17,25 +17,23 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+# Users Petitions
 @router.get("/me/", response_model=User)
 async def read_own_items(
     current_user: Annotated[User, Depends(get_current_active_user)]
 ):
     return current_user
 
-@router.get("/me/items/")
-async def read_own_items(
-    current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
 
+# Admin Petitions
 @router.post("/register")
 async def create_new_user(
     current_user: Annotated[User, Depends(get_current_admin_active_user)],
     new_user: UserInDB
 ):
     #TODO: change code for real DB instead fake one
-    if new_user.username.lower() in fake_users_db:
+    user = user_exist(new_user.username)
+    if type( user ) == dict:
         raise HTTPException(status_code=400, detail="User already Exist")
     
     new_user.hashed_password = get_password_hash(new_user.hashed_password)
@@ -46,8 +44,12 @@ async def create_new_user(
 @router.patch("/recovery-password", status_code=202)
 async def update_user_password(
     current_user: Annotated[User, Depends(get_current_admin_active_user)],
+    username: str,
     password: str,
 ):
+    if not user_exist(username):
+        raise HTTPException(status_code=400,
+                            detail="User Doesn't Exist")
     if len(password) < 8:
         raise HTTPException(status_code=400,
                             detail="Password Lenght should be greater than 8 characters")
@@ -63,3 +65,27 @@ async def update_user_password(
                             detail="")
 
     return {"message": "Password Updated Successfully"}
+
+@router.delete("/delete/{username}", status_code=202)
+async def delete_user(
+    current_user: Annotated[User, Depends(get_current_admin_active_user)],
+    username: str
+):
+    # TODO: Add delete DB function to delete user.
+    return True
+
+
+# General Functions
+def user_exist(username: str) -> bool | dict:
+    """
+        Check if username already exist in db.
+
+        Args:
+            username <str>: The user's Username
+
+        Return:
+            <bool | dict> user dict if user exist and else False.
+    """
+    if username.lower() in fake_users_db:
+        return fake_users_db[username.lower()]
+    return False
